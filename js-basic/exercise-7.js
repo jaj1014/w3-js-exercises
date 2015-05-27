a8bdacb2728819d948c8a491f0f9fbe6e81aa251
Alex Jackson <jaj1014@gmail.com>
2015-05-27 08:00:25 -0400

    Update exercise-7.js

--- js-basic/exercise-7.js
+++ js-basic/exercise-7.js
@@ -1,6 +1,16 @@
 //Exercise 7 Javascript function to find in which year the 1st of January will be a Sunday
-//////I tried to create a function for this exercise.
-//////The loop portion should be enough to accomplish the task
+
+//This example solves the exercise, but isn't reusable. My re-usable version isn't working quite right..
+function getSunday() {
+  
+  for(var year = 2014; year <= 2050; year++) {
+    var date = new Date(year, 0, 1);
+    if(date.getDay() == 0)
+      console.log("January 1st " + year + " is a Sunday.");
+    else
+      console.log("There is no January 1st that's a Sunday in " + year);	
+  }
+}
 
 //////This example doesn't work right now.
 //////Tried in chrome JS console and was getting an error
@@ -12,10 +22,10 @@ function dayByDate(day, date, month, start, end) {
     september: 8, october: 9, november: 10, december: 11};
 
   //Normalize the day/month so they can easily lookup in the tables	
-  var lowerDay = toLowerCase(day);
-  var lowerMonth = toLowerCase(month);
+  var lowerDay = day.toLowerCase();
+  var lowerMonth = month.toLowerCase();
 
-  //Set some validation outs - not all inclusive.
+  //Set some validation 'way out' - not all inclusive.
   //I left out validation for month/date combos
   if(dayTable.lowerDay > 6)
     console.log("Please use a valid day, Sunday through Saturday.");
@@ -23,15 +33,15 @@ function dayByDate(day, date, month, start, end) {
     console.log("Please use a valid month, January through December.");
 
   //Use the start/end as bounds and iterate through them.
-  for(var yr = start; yr <= end; yr+=) {
+  for(var yr = start; yr <= end; yr++) {
     //Pull the date in question by creating new date instance
-    var date = new Date(yr, monthTable.lowerMonth, date);
+    var checkDate = new Date(yr, monthTable.lowerMonth, date);
     //Check to see if the argument date is the current date
-    if(dayTable.lowerDay == date.getDay())
-      //do something if it is
-      console.log(month + " " + date + " " + yr + " is the first " + day + " that will happen between " + start + " and " + end );
-    else
+    if(dayTable.lowerDay == checkDate.getDay())
+    //do something if it is
+ 			console.log(month + " " + date + " " + yr + " is the first " + day + " that will happen between " + start + " and " + end );
+ 		else
       //do something if it isn't
-      console.log("There wasn't a " + day + " on " + month + " " + date + " " + yr + " between " + start + " and " + end);
-  } 
+      console.log("Sorry. That didn't work");
+	}
 }
