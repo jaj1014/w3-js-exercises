d5b24805ce91d23a27f5ac84e5dc7c92c2631110
Alex Jackson <jaj1014@gmail.com>
2015-06-03 12:44:48 -0400

    Create exercise-6.js

---
+++ js-functions/exercise-6.js
@@ -0,0 +1,16 @@
+function longestWord(string) {
+  //put the string in an array
+  var stringArray = string.split(" ");
+  //set up the first word of the array as a var.
+  var longWord = stringArray[0];
+
+  //compare the longWord to each word in the array 
+  //i = 1 since we've already stored stringArray[0] in longWord
+  for(var i = 1; i < stringArray.length; i++)
+    //check to see if the current value stored in longWord is less than each word in stringArray
+    if(longWord.length < stringArray[i].length)
+      //if it's longer, longWord is updated with the new word
+      longWord = stringArray[i];
+
+  return longWord;	
+}
