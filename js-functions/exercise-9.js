fe9cabdffe30319e440dc4e65061238975209b01
Alex Jackson <jaj1014@gmail.com>
2015-06-05 11:08:18 -0400

    Create exercise-9.js

---
+++ js-functions/exercise-9.js
@@ -0,0 +1,9 @@
+// 9. Write a JavaScript function which accepts an argument and returns the type. 
+// Note : There are six possible values that typeof returns: object, boolean, 
+//function, number, string, and undefined.
+
+//seems pointless given the existance of typeof ... 
+function whatIsThis(arg) {
+  return typeof(arg);
+}
+/* Done */
