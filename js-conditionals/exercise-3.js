6daa51d0d5e087144a254242647b2574df7799ec
Alex Jackson <jaj1014@gmail.com>
2015-06-25 16:43:01 -0400

    Create exercise-3.js

---
+++ js-conditionals/exercise-3.js
@@ -0,0 +1,15 @@
+// Sample numbers : -5, -2, -6, 0, -1 
+// Output : 0 
+function greatestOfFive(a, b, c, d, e) {
+  if(a>b && a>c && a>d && a>e) {
+    alert(a);
+  } else if(b>a && b>c && b>d && b>d) {
+    alert(b);
+  } else if(c>a && c>b && c>d && c>e) {
+    alert(c);
+  } else if(d>a && d>b && d>c && d>e) {
+    alert(d);
+  } else {
+    alert(f);
+  }
+}
