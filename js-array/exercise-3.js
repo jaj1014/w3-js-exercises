83b83cc8736350a43125df5f7805fbb4e989e150
Alex Jackson <jaj1014@gmail.com>
2015-07-16 17:56:42 -0400

    Create exercise-3.js

---
+++ js-array/exercise-3.js
@@ -0,0 +1,23 @@
+// 3. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. 
+// Test Data : 
+// console.log(first([7, 9, 0, -2])); 
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output : 
+// 7
+// [] 
+// [7, 9, 0] 
+// [7, 9, 0, -2] 
+// [] 
+function first(arr, n) {
+  if(n === undefined || null) { n = 1;}
+  if(n > arr.length) { n = arr.length;}
+  if(n < 0) {n = 0;}
+
+  for(var i = 0, part = []; i < n; i++) {
+    part.push(arr[i]);
+  }
+  return part;
+}
